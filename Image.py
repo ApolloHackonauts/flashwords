4592a0df651d3a8d76a632356a104ab5af501b6f
adamfarnsworth@umail.ucsb.edu <atfarnsworth@umail.ucsb.edu>
2018-04-21 20:35:30 -0700

    crop image

---
+++ Image.py
@@ -0,0 +1,15 @@
+# import the necessary packages
+import cv2
+
+class Image(object):
+
+    def __init__(self, imageLocation):
+        # saves image location in this object
+        self.image = cv2.imread(imageLocation, 0)
+    
+    def cropAt(self, x, y, w, h, saveLocation):
+        # saves a croped version of the whole image
+        crop_img = self.image[y:y+h, x:x+w]
+        cv2.imwrite(saveLocation, crop_img)
+        
+
