9ee4a785a95bd6abaffb278c8e96606b9cada9ec
adamfarnsworth@umail.ucsb.edu <atfarnsworth@umail.ucsb.edu>
2018-04-22 02:37:25 -0700

    gui 1.0

---
+++ flashCard/app/src/main/java/com/apollo/flashcard/nativeLanguage.java
@@ -0,0 +1,16 @@
+package com.apollo.flashcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class nativeLanguage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_native_language);
+    }
+}
---
+++ flashCard/app/src/main/java/com/apollo/flashcard/targetLanguage.java
@@ -0,0 +1,13 @@
+package com.apollo.flashcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class targetLanguage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_target_language);
+    }
+}
