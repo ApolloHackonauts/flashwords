258808fa87c5410c518d8d6edc592c90afcee31a
Gavin <gavinfrazar@gmail.com>
2018-04-21 20:24:28 -0700

    test tts working

---
+++ ApolloTTS.py
@@ -0,0 +1,30 @@
+#TODO -- add console output logging
+from Languages import langs
+
+def synth_text(text, lang):
+    """Send raw text to be synthesized into audio
+    input:  text : str
+            lang : str
+    output: output.mp3
+    lang must be a valid language code, i.e. 'en-US'
+    """
+    from google.cloud import texttospeech
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.types.SynthesisInput(text=text)
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    response = client.synthesize_speech(input_text, voice, audio_config)
+
+    # The response's audio_content is binary.
+    with open('output.mp3', 'wb') as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+if __name__ == '__main__':
+    synth_text('Yes! Soylent has arrive! Gib right now I want it-I want it!', 'stub')
---
+++ Languages.py
@@ -0,0 +1,10 @@
+languages = {
+    dutch: 'nl-NL',
+    english: 'en-US',
+    french: 'fr-FR',
+    german: 'de-DE',
+    japanese: 'ja-JP',
+    spanish: 'es-ES',
+    swedish: 'sv-SE',
+    turkish: 'tr-TR'
+}
