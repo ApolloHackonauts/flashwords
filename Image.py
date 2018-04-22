6a13c021fa73ee964a1af8bc073a9560e381b44b
adamfarnsworth@umail.ucsb.edu <atfarnsworth@umail.ucsb.edu>
2018-04-21 21:55:49 -0700

    updated

--- Image.py
+++ Image.py
@@ -7,9 +7,11 @@ class Image(object):
         # saves image location in this object
         self.image = cv2.imread(imageLocation, 0)
     
-    def cropAt(self, x, y, w, h, saveLocation):
+    def cropAt(self, x, y, x2, y2, saveLocation='default.jpg'):
         # saves a croped version of the whole image
-        crop_img = self.image[y:y+h, x:x+w]
+        crop_img = self.image[y:y2, x:x2]
         cv2.imwrite(saveLocation, crop_img)
-        
 
+if __name__ == '__main__':
+    pass
+    #test code goes here
